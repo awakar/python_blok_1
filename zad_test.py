f1394ea5cd12bcff771cf9bd72d6386244e2d0a5
awakar <102409020+awakar@users.noreply.github.com>
2022-03-28 12:58:35 +0200

    test

---
+++ zad_test.py
@@ -0,0 +1,7 @@
+imie = input("Podaj Imie: ")
+naswizko = input("Podaj Nazwisko: ")
+wzrost = int(input("Podaj wzrost: "))
+
+print (type (wzrost))
+
+print("Imię: " + imie + "\nNazwisko: " + naswizko + "\nWzrost: " + str(wzrost))
