0d6bf6008d9c072b7def809950311aa9a6a65d19
a_wakarow <andrzej.wakarow@gmail.com>
2022-04-05 00:03:51 +0200

    Merge remote-tracking branch 'origin/main'

--- zad_2.1_aw.py
+++ zad_2.1_aw.py
@@ -5,7 +5,7 @@ licz_b = random.randint(1, 99)
 
 print(f'wylosowano liczby {licz_a} i {licz_b}')
 while True:
-    liczba = int(input('Podaj sumę podanych liczb: '))
-    if liczba == licz_a + licz_b:
+    suma = int(input('Podaj sumę podanych liczb: '))
+    if suma == licz_a + licz_b:
         print('podałeś prawidłową sumę')
         break
