11dd419970632f817e43f6c5f99ba947c0a5dc83
awakar <102409020+awakar@users.noreply.github.com>
2022-04-04 21:09:55 +0200

    Delete .idea directory

---
+++ zad_domowe_1.py
@@ -0,0 +1,18 @@
+
+cena_ban = float(input("podaj cenę banów w zł za kg: "))
+ban_ile = float(input("podaj ile chcesz zakupić bananów w kg: "))
+cena_ziem = float(input("podaj cenę ziemniaków w zł za kg: "))
+ziem_ile = float(input("podaj ile chcesz zakupić ziemniaków w kg: "))
+
+zapl_ban = cena_ban * ban_ile
+zapl_ziemn = cena_ziem + ziem_ile
+
+print(f'do zapłaty  {zapl_ban+zapl_ziemn} zł')
+
+
+if zapl_ban > zapl_ziemn:
+    print('banany były droższe')
+elif ziemniaki > banany:
+    print("ziemniaki były droższe")
+else:
+    print("kupujesz tyle samo za oba")
