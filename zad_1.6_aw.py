f545c3a8120be030833bd054cda53dd2d3d71afe
a_wakarow <andrzej.wakarow@gmail.com>
2022-04-04 23:31:41 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ zad_1.6_aw.py
@@ -0,0 +1,11 @@
+wiek = int(input("Podaj swój wiek: "))\
+il_bilet = int(input("Podaj ile biletów chcesz zakupić: "))
+
+if wiek < 7:
+    cena_biletu = 0
+elif wiek < 18:
+    cena_biletu = 2.28
+elif wiek < 65
+    cena_blietu = 3.8
+else:
+    cena
