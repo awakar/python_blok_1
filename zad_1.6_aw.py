9eb0bd3f325d7a2037f07df53d5c0fc4a816ac99
a_wakarow <andrzej.wakarow@gmail.com>
2022-04-04 23:38:46 +0200

    Merge remote-tracking branch 'origin/main'

--- zad_1.6_aw.py
+++ zad_1.6_aw.py
@@ -1,11 +1,14 @@
-wiek = int(input("Podaj swój wiek: "))\
+wiek = int(input("Podaj swój wiek: "))
 il_bilet = int(input("Podaj ile biletów chcesz zakupić: "))
 
 if wiek < 7:
     cena_biletu = 0
 elif wiek < 18:
     cena_biletu = 2.28
-elif wiek < 65
+elif wiek < 65:
     cena_blietu = 3.8
 else:
-    cena
+    cena_biletu = 1.9
+
+cena = il_bilet * cena_biletu
+print(f'cnea zakupu {il_bilet} biletów to {cena:.2f} zł.')
